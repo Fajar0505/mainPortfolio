7e24fcdcbc5f6956173cbd392a33a4acd4f3f2e4
Fajar0505 <128026823+Fajar0505@users.noreply.github.com>
2023-12-16 07:06:10 +0700

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Smooth Scrolling Animation
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
@@ -15,10 +16,12 @@ $(document).ready(function(){
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
-   
+
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
     } // End if
   });
 });
+
+// #portfolio .project_links h3 effect
